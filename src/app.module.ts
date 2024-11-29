226a718b5cbe758ad10e867474546c3d0eb5eed2
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2024-11-29 23:55:58 +0530

    db connection with env done on local now test on production

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './db/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Make environment variables available globally
+    }),
+    DatabaseModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
