5b5b69f4b68fb2203569e47f90f835f8343fbc76
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2024-11-30 00:25:39 +0530

    db connection 1

--- src/db/database.module.ts
+++ src/db/database.module.ts
@@ -4,22 +4,20 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(), // Ensure environment variables are loaded
+    ConfigModule.forRoot(),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
         const isProduction = configService.get<string>('NODE_ENV') === 'production';
         const uri = isProduction
-          ? configService.get<string>('MONGODB_URI_PROD') // Remote MongoDB URI
-          : configService.get<string>('MONGODB_URI_DEV'); // Local MongoDB URI
-        
+          ? configService.get<string>('MONGODB_URI_PROD')
+          : configService.get<string>('MONGODB_URI_DEV');
+
         console.log(`Connecting to ${isProduction ? 'Production' : 'Local'} database`);
 
         return {
           uri,
-          useNewUrlParser: true, // Ensure compatibility with modern MongoDB drivers
-          useUnifiedTopology: true, // Enable the new server discovery and monitoring engine
         };
       },
     }),
