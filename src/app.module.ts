0d64f89da60fc793d9252b26c795c01a35727000
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2024-11-30 01:34:32 +0530

    db connection 10

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './db/database.module';
+// import { DatabaseModule } from './db/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
--- src/db/database.module.ts
+++ src/db/database.module.ts
@@ -1,25 +1,25 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { Module } from '@nestjs/common';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Module({
-  imports: [
-    ConfigModule.forRoot(), // Ensure environment variables are loaded
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        const isProduction = configService.get<string>('NODE_ENV') === 'production';
-        const uri = isProduction
-          ? configService.get<string>('MONGODB_URI_PROD')  // Production Mongo URI
-          : configService.get<string>('MONGODB_URI_DEV');  // Development Mongo URI
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot(), // Ensure environment variables are loaded
+//     MongooseModule.forRootAsync({
+//       imports: [ConfigModule],
+//       inject: [ConfigService],
+//       useFactory: async (configService: ConfigService) => {
+//         const isProduction = configService.get<string>('NODE_ENV') === 'production';
+//         const uri = isProduction
+//           ? configService.get<string>('MONGODB_URI_PROD')  // Production Mongo URI
+//           : configService.get<string>('MONGODB_URI_DEV');  // Development Mongo URI
 
-        console.log(`Connecting to ${isProduction ? 'Production' : 'Development'} Database`);
+//         console.log(`Connecting to ${isProduction ? 'Production' : 'Development'} Database`);
 
-        return { uri }; // Only pass the URI, no need for useNewUrlParser, useUnifiedTopology
-      },
-    }),
-  ],
-  exports: [MongooseModule],
-})
-export class DatabaseModule {}
+//         return { uri }; // Only pass the URI, no need for useNewUrlParser, useUnifiedTopology
+//       },
+//     }),
+//   ],
+//   exports: [MongooseModule],
+// })
+// export class DatabaseModule {}
